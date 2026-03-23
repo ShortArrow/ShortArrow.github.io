201df570dcbb91099aaf1af4331644898b3b4767
ShortArrow <bamboogeneral@shortarrow.jp>
2026-03-23 21:08:31 +0900

    update: fonts location to static/webfonts

--- setup.ps1
+++ setup.ps1
@@ -1,4 +1,4 @@
 #!pwsh
 npm install @fortawesome/fontawesome-free
-mkdir -p static/fonts
-cp '.\node_modules\@fortawesome\fontawesome-free\webfonts\*' .\static\fonts\
+mkdir -p static/webfonts
+cp .\node_modules\@fortawesome\fontawesome-free\webfonts\* .\static\webfonts\
